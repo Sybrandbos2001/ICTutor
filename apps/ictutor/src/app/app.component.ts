b98f3ca746bfc5abef739e7712a9ec4484be13f0
Sybrand B <Sybrandbos@gmail.com>
2024-01-08 22:26:11 +0100

    Navbar + Landing

--- apps/ictutor/src/app/app.component.ts
+++ apps/ictutor/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NavbarComponent, FooterComponent],
   selector: 'ictutor-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ictutor';
+  title = 'ICTutor';
 }
--- apps/ictutor/src/app/app.routes.ts
+++ apps/ictutor/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: '', pathMatch: 'full', redirectTo: 'landing' },
+    {
+        path: 'landing',
+        pathMatch: 'full',
+        component: LandingComponent,
+      },
+];
