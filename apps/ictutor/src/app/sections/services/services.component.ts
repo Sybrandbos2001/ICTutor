8c61b3ac68d9ba86bf3e1456e17f81f23d750199
Sybrand B. <snjw.bos@student.avans.nl>
2024-07-10 15:31:05 +0200

    Individual cards

--- apps/ictutor/src/app/sections/services/services.component.ts
+++ apps/ictutor/src/app/sections/services/services.component.ts
@@ -14,32 +14,32 @@ export class ServicesComponent {
   cards: Card[] = [
     {
       title: 'Laptop',
-      description: 'Test description for Laptop',
+      description: 'Ervaart u problemen met uw laptop? Software die niet installeert of bevat hij mogelijk een virus? Wij kunnen u helpen dit op te lossen zodat u weer snel aan de slag kunt.',
       image_url: '/assets/images/card_images/laptop_image.png'
     },
     {
       title: 'Printer',
-      description: 'Test description for Printer',
+      description: 'Heeft u printerproblemen? Papierstoringen, slechte afdrukken of connectieissues? Wij lossen het snel voor u op.',
       image_url: '/assets/images/card_images/printer_image.png'
     },
     {
       title: 'Telefoon',
-      description: 'Test description for Telefoon',
+      description: 'Heeft u telefoonproblemen? Apps crashen, batterij snel leeg of connectieproblemen? Wij zorgen dat uw telefoon weer naar behoren werkt.',
       image_url: '/assets/images/card_images/phone_image.png'
     },
     {
       title: 'Tablet',
-      description: 'Test description for Tablet',
+      description: 'Heeft u problemen met uw tablet? Schermreactie, oplaadissues of app-problemen? Wij helpen graag om uw tablet weer perfect te laten functioneren.',
       image_url: '/assets/images/card_images/tablet_image.png'
     },
     {
       title: 'Internet',
-      description: 'Test description for Internet',
+      description: 'Heeft u internet- of wifi-problemen? Trage verbinding, signaaluitval of helemaal geen verbinding? Wij zorgen voor een stabiele en snelle internetervaring.',
       image_url: '/assets/images/card_images/internet_image.png'
     },
     {
       title: 'Overig',
-      description: 'Test description for Internet',
+      description: 'Heeft u andere IT-gerelateerde vragen of problemen? Van software-installaties tot hardware-hulp, wij bieden de oplossing om u weer op weg te helpen.',
       image_url: '/assets/images/card_images/other_image.png'
     }
   ];
